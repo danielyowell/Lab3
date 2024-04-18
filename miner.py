c36be6318d6e72825907dde68751a32ba784f8ad
Daniel Yowell <danielyowell@ou.edu>
2024-04-18 17:49:09 -0500

    current miner is slow; going to try to optimize

--- miner.py
+++ miner.py
@@ -5,6 +5,15 @@ import requests
 import hashlib
 import json
 import time
+from random import randint
+
+'''
+generate random 10 digit number
+'''
+def random_with_N_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
 
 """
 calculate the hash value of a block
@@ -13,36 +22,18 @@ def calculate_hash(index, previous_hash, timestamp, data, nonce):
     block_header = str(index) + previous_hash + str(timestamp) + data + str(nonce)
     return hashlib.sha256(block_header.encode()).hexdigest()
 
-"""
-mine a block.
-"""
-def mine_block(index, previous_hash, timestamp, data, difficulty):
-    prefix = '0' * difficulty
-    nonce = 0
-    while True:
-        block_hash = calculate_hash(index, previous_hash, timestamp, data, nonce)
-        if block_hash.startswith(prefix):
-            print("Nonce found:", nonce)
-            print("Block hash with 7 leading zeros:", block_hash)
-            return nonce, block_hash
-        nonce += 1
-
-
-"""
-create a block and send it to the sooners php
-"""
-def generateNewBlock(index, currentHash, timestamp, data, difficulty):
-    print("currentHash:", currentHash)
-    print("(This will be the previousHash of the block we are mining)")
-    nonce, block_hash = mine_block(index, currentHash, timestamp, data, difficulty)
-
+'''
+congratulations! your hash met the difficulty requirement.
+now pack it up and submit
+'''
+def submitBlock(index, currentHash, timestamp, nonce, newHash):
     payload = {
         "index": int(index),
         "previousHash": str(currentHash),
-        "timestamp": timestamp,
+        "timestamp": time.time(),
         "data": "Daniel Yowell",
         "nonce": int(nonce),
-        "hash": str(block_hash)
+        "hash": str(newHash)
     }
 
     print("===PAYLOAD===")
@@ -68,38 +59,37 @@ def getData(block):
 '''
 main
 '''
-def main():
-    url = 'https://miners.sooners.us/latest_block.php'
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        block = response.json()
-        # get index, timestamp, data, and CURRENT hash
-        index, timestamp, data, currentHash = getData(block)
-        difficulty = 7
-        generateNewBlock(index, currentHash, timestamp, data, difficulty)
-    else:
-        print(f"Request failed with status code {response.status_code}")
-
 if __name__ == "__main__":
     #main()
     url = 'https://miners.sooners.us/latest_block.php'
-    
+    difficulty = 7
+
     previousHash = "[BEGIN_PROGRAM]"
     currentHash = ""
-    
+    nonce = random_with_N_digits(10)
+    prefix = '0' * difficulty
+
     while True:
         response = requests.get(url)    
         if response.status_code == 200:
             block = response.json()
             # get index, timestamp, data, and CURRENT hash
             index, timestamp, data, currentHash = getData(block)
-            # try to generate a new hash:
-            # generateHash() # 
-            # if hash = 7zeroes: # check for 7 zeroes
+            
+            # did the hash change? if so, reset nonce
             if currentHash != previousHash:
                 print("hash change: was ", previousHash, ", now ", currentHash, sep="")
+                #nonce = 0
+            
+            # try to generate a new hash:
+            newHash = calculate_hash(index, currentHash, timestamp, data, nonce)            
+            if newHash.startswith(prefix):
+                print("Nonce found:", nonce)
+                print("Block hash with 7 leading zeros:", newHash)
+                submitBlock(index, currentHash, timestamp, nonce, newHash)     
+            
+            # increment nonce
+            nonce = random_with_N_digits(10)
         else:
             print(f"Request failed with status code {response.status_code}")
         previousHash = currentHash
-        # time.sleep(3)
---
+++ timestamping.py
@@ -0,0 +1,5 @@
+import time
+
+# Get the current Unix timestamp
+current_timestamp = time.time()
+print("Current timestamp:", current_timestamp)
